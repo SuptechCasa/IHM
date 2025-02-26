a2ff1577590e1a6bb5931ec770c38844d9ea056e
enaanai <aenaanai@uae.ac.ma>
2025-02-26 12:11:15 +0000

    Initial commit

---
+++ src/Principale.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
+import fenetres.Fenetre;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		Fenetre fenetre1=new Fenetre(new Dimension(600, 500), new Color(173, 249, 250));
+
+	}
+
+}
---
+++ src/fenetres/Fenetre.java
@@ -0,0 +1,36 @@
+package fenetres;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Fenetre extends JFrame {
+public Fenetre(Dimension dimension, Color color) {
+	setSize(dimension);
+	setLocationRelativeTo(null);//Centrer la fenêtre
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	getContentPane().setBackground(color);
+	//Définir la disposition
+	getContentPane().setLayout(new BorderLayout());
+	//Créer des panneaux
+	JPanel nord=new JPanel();nord.setBackground(Color.blue);
+	JPanel sud=new JPanel();sud.setBackground(Color.yellow);
+	JPanel est=new JPanel();est.setBackground(Color.green);
+	JPanel ouest=new JPanel();ouest.setBackground(Color.RED);
+	JPanel centre=new JPanel();centre.setBackground(Color.WHITE);
+	//Ajouter les panneaux à la fenêtre
+	getContentPane().add(nord,BorderLayout.NORTH);
+	getContentPane().add(sud,BorderLayout.SOUTH);
+	getContentPane().add(est,BorderLayout.EAST);
+	getContentPane().add(ouest,BorderLayout.WEST);
+	getContentPane().add(centre,BorderLayout.CENTER);
+	setVisible(true);
+}
+}
