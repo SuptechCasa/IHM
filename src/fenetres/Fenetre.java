9bb4bb7b7eb89da8ddb4a7ed51d0abc849c036b7
Erramielmehdi <elmehdierrami89@gmail.com>
2025-02-27 15:54:09 +0000

    errami

--- src/fenetres/Fenetre.java
+++ src/fenetres/Fenetre.java
@@ -1,15 +1,12 @@
 package fenetres;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Fenetre extends JFrame {
 public Fenetre(Dimension dimension, Color color) {
@@ -19,32 +16,42 @@ public Fenetre(Dimension dimension, Color color) {
 	getContentPane().setBackground(color);
 	//Définir la disposition
 	getContentPane().setLayout(new BorderLayout());
-	//Créer des panneaux
-	JPanel nord = new JPanel();
+	// Create a border for panels
+    Border panelBorder = BorderFactory.createLineBorder(Color.BLACK, 2);
+
+    // Create panels with borders
+    JPanel nord = new JPanel();
     nord.setBackground(Color.blue);
-    nord.setPreferredSize(new Dimension(dimension.width, 50)); // Fixed height
+    nord.setPreferredSize(new Dimension(dimension.width, 50));
+    nord.setBorder(panelBorder);
 
     JPanel sud = new JPanel();
     sud.setBackground(Color.yellow);
-    sud.setPreferredSize(new Dimension(dimension.width, 50)); // Fixed height
+    sud.setPreferredSize(new Dimension(dimension.width, 50));
+    sud.setBorder(panelBorder);
 
     JPanel est = new JPanel();
     est.setBackground(Color.green);
-    est.setPreferredSize(new Dimension(100, dimension.height - 100)); // Fixed width
+    est.setPreferredSize(new Dimension(100, dimension.height - 100));
+    est.setBorder(panelBorder);
 
     JPanel ouest = new JPanel();
     ouest.setBackground(Color.RED);
-    ouest.setPreferredSize(new Dimension(100, dimension.height - 100)); // Fixed width
+    ouest.setPreferredSize(new Dimension(100, dimension.height - 100));
+    ouest.setBorder(panelBorder);
 
     JPanel centre = new JPanel();
     centre.setBackground(Color.WHITE);
-    
-    
-	getContentPane().add(nord,BorderLayout.NORTH);
-	getContentPane().add(sud,BorderLayout.SOUTH);
-	getContentPane().add(est,BorderLayout.EAST);
-	getContentPane().add(ouest,BorderLayout.WEST);
-	getContentPane().add(centre,BorderLayout.CENTER);
+    centre.setBorder(panelBorder);
+    // No preferred size needed, it will take the remaining space
+
+    // Add panels to the window
+    getContentPane().add(nord, BorderLayout.NORTH);
+    getContentPane().add(sud, BorderLayout.SOUTH);
+    getContentPane().add(est, BorderLayout.EAST);
+    getContentPane().add(ouest, BorderLayout.WEST);
+    getContentPane().add(centre, BorderLayout.CENTER);
+
 	setVisible(true);
 }
 }
