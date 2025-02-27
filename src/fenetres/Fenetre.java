0070f05bc265498fc94a5a47a262a7f2957682cf
Malick984 <m.gaye@hestim.ma>
2025-02-27 15:22:18 +0000

    aaa

--- src/fenetres/Fenetre.java
+++ src/fenetres/Fenetre.java
@@ -25,7 +25,7 @@ public Fenetre(Dimension dimension, Color color) {
 	JPanel est=new JPanel();est.setBackground(Color.green);
 	JPanel ouest=new JPanel();ouest.setBackground(Color.RED);
 	JPanel centre=new JPanel();centre.setBackground(Color.WHITE);
-	//Ajouter les panneaux à la fenêtre
+	//Ajouter les panneaux à la fenêtrehhh
 	getContentPane().add(nord,BorderLayout.NORTH);
 	getContentPane().add(sud,BorderLayout.SOUTH);
 	getContentPane().add(est,BorderLayout.EAST);
